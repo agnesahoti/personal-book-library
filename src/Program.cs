269c00b8c834beaf1e094c979140cbd0a377a404
Agnesa Hoti <hotiagnesa90@gmail.com>
2026-03-19 08:56:01 +0100

    Add minimal Program.cs for application initialization

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PersonalBookLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Personal Book Library Application Started");
+        }
+    }
+}

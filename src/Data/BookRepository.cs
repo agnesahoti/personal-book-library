bf7c325e73e6f27779ed59ceffc9cdbef93d3650
Agnesa Hoti <hotiagnesa90@gmail.com>
2026-03-20 12:14:19 +0100

    Complete repository pattern implementation

--- src/Data/BookRepository.cs
+++ src/Data/BookRepository.cs
@@ -3,13 +3,18 @@ using PersonalBookLibrary.Models;
 
 namespace PersonalBookLibrary.Data
 {
-    public class BookRepository
+    public class BookRepository : IRepository<Book>
     {
         private List<Book> books = new List<Book>();
 
         public List<Book> GetAll()
         {
             return books;
+        }
+
+        public Book GetById(int id)
+        {
+            return books.Find(b => b.GetId() == id);
         }
 
         public void Add(Book book)

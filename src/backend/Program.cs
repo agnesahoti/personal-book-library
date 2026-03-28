79fd6d417ddb2c4b1cc0fe5be9f439122d49f6bb
Agnesa Hoti <hotiagnesa90@gmail.com>
2026-03-28 12:57:07 +0100

    Add book from user input and save to CSV

--- src/backend/Program.cs
+++ src/backend/Program.cs
@@ -1,19 +1,35 @@
 using System;
+using PersonalBookLibrary.Models;
 using PersonalBookLibrary.Data;
 
-namespace PersonalBookLibrary
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var repo = new FileRepository();
-            var books = repo.GetAll();
+        FileRepository repo = new FileRepository();
 
-            foreach (var b in books)
-            {
-                Console.WriteLine($"{b.GetId()} - {b.GetTitle()} - {b.GetAuthor()}");
-            }
+        Console.WriteLine("Enter book title:");
+        string title = Console.ReadLine();
+
+        Console.WriteLine("Enter book author:");
+        string author = Console.ReadLine();
+
+        // krijo ID automatik
+        var books = repo.GetAll();
+        int newId = books.Count + 1;
+
+        Book newBook = new Book();
+        newBook.SetId(newId);
+        newBook.SetTitle(title);
+        newBook.SetAuthor(author);
+
+        repo.Add(newBook);
+
+        Console.WriteLine("\nBooks in library:\n");
+
+        foreach (var book in repo.GetAll())
+        {
+            Console.WriteLine($"{book.GetId()} - {book.GetTitle()} - {book.GetAuthor()}");
         }
     }
 }
